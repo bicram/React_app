df4de1a7551390df383a29f65f6ac69b5ead3835
bikram sarker <sarkerbikram@gmail.com>
2019-01-11 13:59:16 +0600

    working starts from here

---
+++ App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
+import './App.css';
+
+class App extends Component {
+  render() {
+
+
+    return (
+      <div className="App">
+        <Header/>
+        
+        <Contact/>
+
+      </div>
+    );
+  }
+}
+export default App;
---
+++ components/Contact.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>The app component</h4>
+      	<ul>
+      		<li>email:fihi</li>
+      		<li>phone:siojhojo</li>
+      	</ul>
+      </div>
+    );
+  }
+}
+export default Contact;
+
+
